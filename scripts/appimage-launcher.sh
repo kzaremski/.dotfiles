62c05886fe7fc06d525b68106f8778fe5393441f
Konstantin Zaremski <konstantin.zaremski@gmail.com>
2024-07-02 21:18:35 -0600

    Simple dmenu-based AppImage launcher

---
+++ scripts/appimage-launcher.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+chosenAppImage=$(ls $HOME/Applications/ | sed -e 's/\.AppImage$//' | dmenu -fn "CaskaydiaCove Nerd Font Mono-10")
+if [[ "$(expr length "$chosenAppImage")" != "0" ]]
+then
+    $HOME/Applications/$chosenAppImage
+	exit
+fi
+echo "No AppImage file selected."
+
+
---
+++ scripts/install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# AppImage Launcher
+chmod +x $HOME/.dotfiles/scripts/appimage-launcher.sh
+ln -s $HOME/.dotfiles/scripts/appimage-launcher.sh /usr/bin/appimage-launcher
+
+# Screen Layout Selector
+chmod +x $HOME/.dotfiles/scripts/screen-layout-selector.sh
+ln -s $HOME/.dotfiles/scripts/screen-layout-selector.sh /usr/bin/screen-layout-selector
+
