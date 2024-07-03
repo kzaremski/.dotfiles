1d214f9eb2abf59adcb404ff67b9415632b55228
Konstantin Zaremski <konstantin.zaremski@gmail.com>
2024-07-02 21:20:58 -0600

    Fixed script not launching AppImages

--- scripts/appimage-launcher.sh
+++ scripts/appimage-launcher.sh
@@ -2,7 +2,7 @@
 chosenAppImage=$(ls $HOME/Applications/ | sed -e 's/\.AppImage$//' | dmenu -fn "CaskaydiaCove Nerd Font Mono-10")
 if [[ "$(expr length "$chosenAppImage")" != "0" ]]
 then
-    $HOME/Applications/$chosenAppImage
+    $HOME/Applications/$chosenAppImage.AppImage
 	exit
 fi
 echo "No AppImage file selected."
